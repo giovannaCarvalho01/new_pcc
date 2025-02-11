464c02fead90f26858ebfb0fd4016089e6bb8b28
giovannaCarvalho01 <giovannavital45@gmail.com>
2025-02-11 20:52:57 -0300

    fix: ajustando endpoint

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 export const API_BASE_URL_DEV = "http://localhost:3001";
 
 // export const API_BASE_URL_PRD = "http://localhost:3001/";
-export const API_BASE_URL_PRD = "https://api-enade-870qjzwrp-giovannacarvalho01s-projects.vercel.app/";
+export const API_BASE_URL_PRD = "https://api-enade-27u9k3kqq-giovannacarvalho01s-projects.vercel.app/";
