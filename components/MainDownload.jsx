869da627eee64e6f42650a120d7df443f796adfd
giovannaCarvalho01 <giovannavital45@gmail.com>
2025-02-12 20:42:25 -0300

    feat: download csv

--- components/MainDownload.jsx
+++ components/MainDownload.jsx
@@ -47,11 +47,61 @@ export default function MainDownload({ filters }) {
     }
   };
 
+  const handleDownloadCsv = async () => {
+    if (!filters) {
+      alert("Por favor, aplique os filtros antes de exportar.");
+      return;
+    }
+  
+    setIsDownloading(true); // Exibe o popup de carregamento
+  
+    try {
+      // Mapeia os filtros para os parâmetros esperados pela API
+      const queryParams = new URLSearchParams({
+        ano: filters.ano,
+        regiao: filters.regiao,
+        uf: filters.uf,
+        municipio: filters.municipio,
+        cat_adm: filters.catAdm,
+        cod_ies: filters.ies,
+        grp: filters.curso,
+        presenca: 555,
+      }).toString();
+  
+      const response = await axios.get(`${API_BASE_URL_PRD}notas/download/csv?${queryParams}`, {
+        responseType: "blob", // Assegura que estamos recebendo os dados como um arquivo binário
+      });
+  
+      // Verifica se o tipo de conteúdo é 'csv'
+      const contentType = response.headers["content-type"];
+      const fileExtension = contentType && contentType.includes("csv") ? ".csv" : ".xlsx"; // Garante o tipo correto
+  
+      // Cria um link para download
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+  
+      // Define o nome do arquivo com base no tipo de conteúdo
+      link.setAttribute("download", `notas_${Date.now()}${fileExtension}`);
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    } catch (error) {
+      console.error("Erro ao exportar para CSV:", error);
+      alert("Ocorreu um erro ao tentar exportar os dados. Tente novamente.");
+    } finally {
+      setIsDownloading(false); // Esconde o popup de carregamento
+    }
+  };
+  
   return (
     <div className="MainDownload">
       <div className="export">
         <button className="exportButton" onClick={handleDownload}>
           Exportar para Xlsx
+        </button>
+        <button className="exportButton" onClick={handleDownloadCsv}>
+          Exportar para Csv
         </button>
       </div>
       {filters ? (
--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 export const API_BASE_URL_DEV = "http://localhost:3001";
 
 // export const API_BASE_URL_PRD = "http://localhost:3001/";
-export const API_BASE_URL_PRD = "https://api-enade-qzcq1iyef-giovannacarvalho01s-projects.vercel.app/";
+export const API_BASE_URL_PRD = "https://api-enade-epzp8gke4-giovannacarvalho01s-projects.vercel.app/";
